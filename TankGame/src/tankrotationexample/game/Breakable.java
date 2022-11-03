a296f3ab100bb0c13199b03b05e4b60b5c81fce6
meganlew <meganlew4@gmail.com>
2022-11-02 22:17:11 -0700

    wall color

--- TankGame/src/tankrotationexample/game/Breakable.java
+++ TankGame/src/tankrotationexample/game/Breakable.java
@@ -1,13 +1,22 @@
 package tankrotationexample.game;
 
+import java.awt.*;
 import java.awt.image.BufferedImage;
 
-public class Breakable extends Wall{
+public class Breakable extends Wall {
 
     int state = 2;
 
-    public Breakable(float x, float y, BufferedImage img){
-        super(x,y,img);
+    public Breakable(float x, float y, BufferedImage img) {
+        super(x, y, img);
+    }
+
+    public void drawImage(Graphics g) {
+
+        Graphics2D g2d = (Graphics2D) g;
+        g.drawImage(this.img, (int) x, (int) y, null);
+        g2d.setColor(Color.BLUE);
+        g2d.drawRect((int) x, (int) y, this.img.getWidth(), this.img.getHeight());
     }
 }
 
